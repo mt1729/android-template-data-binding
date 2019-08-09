89da200fb5542a13041b56df1c533a935cec3bfa
Matthew Taylor <mtaylor@surterra.com>
2019-08-09 11:36:05 -0400

    Add TODO

--- app/src/main/java/com/company/app/app/appModule.kt
+++ app/src/main/java/com/company/app/app/appModule.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.asCoroutineDispatcher
 import org.koin.dsl.module
 import kotlin.coroutines.CoroutineContext
 
+// TODO: - Dynamic feature modularization (figure out what needs to be in core)
 internal val appModule = module {
     // Using THREAD_POOL_EXECUTOR for the sake of passing Espresso tests (implicit IdlingResource)
     single<CoroutineContext> { AsyncTask.THREAD_POOL_EXECUTOR.asCoroutineDispatcher() }
